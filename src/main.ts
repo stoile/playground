dda3900b1bcbae9b7be0bebf76fd333db2a00a06
Philipp Riegger <philipp.riegger@itemis.com>
2018-02-16 13:51:34 +0100

    Fix tslint errors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ interface IStartup {
 
 class Startup implements IStartup {
     public main(): number {
+        // tslint:disable-next-line:no-console
         console.log("Hello World")
         return 0
     }
