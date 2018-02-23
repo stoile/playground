26ff6285ec6c79f731c01b93319d7060788c3a14
Philipp Riegger <philipp.riegger@itemis.com>
2018-02-23 14:54:28 +0100

    Query the GraphQL endpoint

--- ts-client/src/main.ts
+++ ts-client/src/main.ts
@@ -1,13 +1,28 @@
-interface IStartup {
-    main(): number
-}
+import { InMemoryCache } from "apollo-cache-inmemory"
+import { ApolloClient } from "apollo-client"
+import { HttpLink } from "apollo-link-http"
+import fetch from "isomorphic-unfetch"
 
-class Startup implements IStartup {
-    public main(): number {
-        // tslint:disable-next-line:no-console
-        console.log("Hello World")
-        return 0
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "http://localhost:3000/graphql", fetch }),
+  cache: new InMemoryCache()
+})
+
+import gql from "graphql-tag"
+
+client.query({
+  query: gql`
+    query {
+      author(firstName:"Edmond", lastName: "Jones"){
+        firstName
+        lastName
+        posts{
+          title
+          views
+        }
+      }
     }
-}
-
-new Startup().main()
+  `,
+})
+  .then(data => console.log(data))
+  .catch(error => console.error(error))
