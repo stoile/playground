03f4270b21ab5fd175a97c36bf8b2292ef1f4620
Philipp Riegger <philipp.riegger@itemis.com>
2018-02-23 09:31:32 +0100

    Add the Author and Post types and mocking

--- graphql-server/data/schema.js
+++ graphql-server/data/schema.js
@@ -3,7 +3,24 @@ import mocks from './mocks';
 
 const typeDefs = `
 type Query {
-  testString: String
+  author(firstName: String, lastName: String): Author
+  allAuthors: [Author]
+  getFortuneCookie: String # we'll use this later
+}
+
+type Author {
+  id: Int
+  firstName: String
+  lastName: String
+  posts: [Post]
+}
+
+type Post {
+  id: Int
+  title: String
+  text: String
+  views: Int
+  author: Author
 }
 `;
 
